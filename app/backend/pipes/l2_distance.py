a3f4ffa6ebf68b4937a639ac8b0f57bb5646927e
Brian Coe <bwc126@gmail.com>
2023-03-10 18:00:55 -0500

    feat: Continue implementing distance function

--- app/backend/pipes/l2_distance.py
+++ app/backend/pipes/l2_distance.py
@@ -4,6 +4,7 @@ from flask import jsonify
 from classes import Person
 from classes import Location
 from classes import Person_Location
+from sqlalchemy import Select
 
 # Calculate the L2 distance between two locations given their names.
 # Lookup the names.
@@ -11,8 +12,18 @@ from classes import Person_Location
 # Return the Euclidean or L2 distance between them. 
 
 def l2_distance(person_1, person_2):
-    loc_1 = Person_Location.query.get(person_id=person_1)
-    loc_2 = Person_Location.query.get(person_id=person_2) 
+    loc_ids = []
+    for person in [person_1, person_2]:
+        loc_ids.append(Select(Person_Location).where(Person_Location.person_id == person).location_id)
+    # loc_2 = Select(Person_Location).where(Person_Location.person_id == person_2).location_id
+    locs = []
+    for loc in loc_ids:
+        locs.append(Location.query.get(loc))
+
+    coords = [(loc.longitude, loc.latitude) for loc in locs]
+    # loc_1 = Person_Location.query.get(person_id=person_1)
+    # loc_1 = Person_Location.query.where
+    # loc_2 = Person_Location.query.where(person_id=person_2) 
     # IDEA: grab the locations for both the above person-locations
     pass 
 
